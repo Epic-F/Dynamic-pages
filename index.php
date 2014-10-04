353b643766514f4a8478c76f360686b65f15729e
Fadi Abdalkhalk <fadiabdalkhalk@gmail.com>
2014-10-04 13:24:37 +0200

    Source
    Source code it self

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ *			config.php
+ *            .---._------------------------------+
+ *___________/ ._____)                            |
+ *              ) __|         created by          |
+ *                __|      Fadi Abdalkhalk        |
+ *..---------.._____|                             |
+ *                  +-----------------------------+
+ *
+ *	copyright 2014 - Gitcloud
+ *	created on 13-06-2014
+ *	email: fadiabdalkhalk@gmail.com
+ * 
+ */
+ 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+
+
+// includes the config file. 
+include('app/library/config.inc');
+
+
+// includes the dynamic routing file.
+include('app/routes.inc.php');
+?>
